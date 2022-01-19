163eb1a4629045d30c6630b529e5d80fa8855939
henry <48340699+imwh0im@users.noreply.github.com>
2022-01-19 14:22:18 +0900

    chore: upbit api

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ import * as crypto from 'crypto';
 import * as jwt from 'jsonwebtoken';
 import * as qs from 'query-string';
 
-import { JwtPaylaod } from '@types';
+import {
+  GetAccountsResponse,
+  GetOrdersChanceRequestBody,
+  GetOrdersChanceResponse,
+  JwtPaylaod,
+} from '@types';
+import axios, { AxiosResponse } from 'axios';
 import { v4 as uuidV4 } from 'uuid';
 
 /**
@@ -16,17 +22,52 @@ export class ApiUpbit {
   constructor(
     private readonly AccessKey: string,
     private readonly SecretKey: string,
-  ) {
-    if (!ApiUpbit.instance) {
-      ApiUpbit.instance = this;
-    }
-    return ApiUpbit.instance;
+  ) {}
+
+  private readonly UpbitHost = 'https://api.upbit.com/v1';
+
+  /**
+   * Show List of assets you have.
+   * `/v1/accounts`
+   * https://docs.upbit.com/reference/%EC%A0%84%EC%B2%B4-%EA%B3%84%EC%A2%8C-%EC%A1%B0%ED%9A%8C
+   */
+  public async getAccounts(): Promise<GetAccountsResponse[]> {
+    const bearerToken = this.makeJwtToken();
+    const res = <AxiosResponse<GetAccountsResponse[]>>await axios({
+      method: 'GET',
+      headers: {
+        Authorization: bearerToken,
+      },
+      url: `${this.UpbitHost}/accounts`,
+    });
+
+    return res.data;
   }
 
   /**
-   * For Singleton Pattern
+   * Check the order availability information by market.
+   * `/v1/orders/chance`
+   * https://docs.upbit.com/reference/%EC%A3%BC%EB%AC%B8-%EA%B0%80%EB%8A%A5-%EC%A0%95%EB%B3%B4
    */
-  private static instance: ApiUpbit;
+  public async getOrdersChance(
+    market: string,
+  ): Promise<GetOrdersChanceResponse> {
+    const body: GetOrdersChanceRequestBody = {
+      market: market,
+    };
+    const bearerToken = this.makeJwtToken(body);
+
+    const res = <AxiosResponse<GetOrdersChanceResponse>>await axios({
+      method: 'GET',
+      url: `${this.UpbitHost}/orders/chance`,
+      headers: {
+        Authorization: bearerToken,
+      },
+      data: body,
+    });
+
+    return res.data;
+  }
 
   /**
    * Seceret Key is JwtSecret
--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,4 @@
 export * from './jwt-payload.inteface';
+export * from './get-accounts-response.interface';
+export * from './get-orders-chance-request-body.interface';
+export * from './get-orders-chance-response.interface';
